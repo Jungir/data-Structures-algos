97b6a9ec84734e35849a6f8713a3d97363497ee2
Jungir <googlman6300@gmail.com>
2019-09-06 16:02:51 +0600

    push/pop, unshift/shift

--- dataStructures/doubleLinledList.js
+++ dataStructures/doubleLinledList.js
@@ -5,6 +5,8 @@ class Node{
         this.next = null;
         this.prev = null;
     }
+    
+
 }
 
 //Double linked list
@@ -16,5 +18,68 @@ class DoublyLinkedList{
         this.tail = null;
         this.length = 0;
     }
-    
+    push(val){
+        let newNode = new Node(val);
+        if(this.head === null){
+            this.head = newNode;
+            this.tail = newNode;
+          
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail; 
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+
+    }
+    pop(){
+        if(!this.tail) return undefined;
+        let poppedNode = this.tail;
+        if(this.length < 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
+    }
+    shift(){
+        if(!this.head) return undefined;
+        let oldHead = this.head;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = oldHead.next;
+            //sever the connections
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+    unshift(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+
+const list = new DoublyLinkedList();
+
+list.push(1);
+list.push(2);
+list.push(3);
+console.log(list)
